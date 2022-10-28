14a53bbca1ff4531f77ad55b3c19939c43f9d026
AmgadSa <AmgadSa@outlook.com>
2022-10-28 16:39:51 +0300

    Mark exercise 05 as solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(number1,number2) {
+    if (typeof (number1+number2) != typeof 1 || number1*number2<0 ){
+        return 'ERROR'
+    }
+    let greaterNumber =  (number1>number2) ? number1 : number2;
+    let lesserNumber = (number1<number2) ? number1 : number2;
+    let sum = 0;
+    for(let i=lesserNumber; i<=greaterNumber; i++){
+        sum+= i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
