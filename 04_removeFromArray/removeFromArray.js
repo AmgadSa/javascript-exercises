ea18250d3f5edceca7022690799be008d84d6c29
AmgadSa <AmgadSa@outlook.com>
2022-10-28 15:54:01 +0300

    Mark exercise 04 as solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    let changedArray = [...args[0]];
+    let index;
+    for (let i=0 ;i<args.length-1;i++){
+        index = changedArray.indexOf(args[i+1]);
+        (index >= 0) ? changedArray.splice(index,1):':)';
+    }
 
+    return changedArray
 };
 
 // Do not edit below this line
