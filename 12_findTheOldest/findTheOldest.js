4ea7ef6650dc49f80c83efc1f4b6183e1a635d14
AmgadSa <AmgadSa@outlook.com>
2022-11-02 22:21:50 +0300

    Mark exercise 12 as solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(list) {
+    return list.reduce((y,x)=>{
+        let year = new Date().getFullYear();
+        let age1 = (x.yearOfDeath || year)-x.yearOfBirth;
+        let age2 = (y.yearOfDeath || year)-(y.yearOfBirth || year);
+        return (age1) > (age2) ? x : y;
+    }
+    , {})
 };
 
 // Do not edit below this line
