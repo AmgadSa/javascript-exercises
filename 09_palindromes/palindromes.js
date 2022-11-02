18e34945445deb4235b222d933db5bb7b0238b8d
AmgadSa <AmgadSa@outlook.com>
2022-11-02 20:07:01 +0300

    Mark exercise 09 as solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let pal = str.toLowerCase().split('').filter(x=>/\w/g.test(x));
+    return pal.join('')==[...pal].reverse().join('');
 };
 
 // Do not edit below this line
