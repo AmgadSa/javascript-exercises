8e32d78744dc019f9661d654d1f1876d358f424d
AmgadSa <AmgadSa@outlook.com>
2022-11-02 20:57:47 +0300

    Mark exercise 10 as solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    let y = [1,1,2];
+    while(y.length<x){
+        y =[...y,y[y.length-2]+y[y.length-1]];
+    }
+    console.log(y);
+    return y[x-1] || "OOPS";
 };
 
 // Do not edit below this line
